7a1de6b8ee6ba8599eaf09d35d4ca6ac698662af
josergb <lole@thebighouse.io>
2015-09-30 21:01:49 +0200

    Refactoring code and optimization of output css

--- js/index.js
+++ js/index.js
@@ -1,4 +1,32 @@
-var i, u = 0;
+
+//vars and conf
+var currentClass = '';
+
+var single = 16;
+var middle = single / 2;
+
+var confFixed = {
+  'flex': {
+    'width': ['1', '2'],
+    'attr': {
+      'rotate': {
+        'justifyContent': ['center'],
+        'alignContent': ['center'],
+        'flexDirection': ['row']
+      },
+      'fixed': {
+        'margin': {
+          'marginRight': [middle],
+          'marginLeft': [middle],
+          'marginTop': [single, middle]
+        },
+        'padding': {
+          'padding': [middle, single]
+        },
+      }
+    }
+  }
+}
 
 //Functions
 function toTitleCase(str) {
@@ -29,24 +57,52 @@ function recorrerJsonLevel(lvl) {
   });
 };
 
+
 //Iteration for json
 function recorrerObj(objetive, key) {
   classFixed = key;
+ 
+  //working with name class in basic case, for example MR8 = marginRight:8;
+  classFixedUppercaseFirstLetter  = classFixed.charAt(0).toUpperCase() + classFixed.slice(1);
+  classFixedArray                 = classFixedUppercaseFirstLetter.split(/(?=[A-Z])/);
+  classFixedSimple                = ( classFixedArray.length > 1 ) ? classFixedArray[0].charAt(0) + classFixedArray[1].charAt(0) : classFixedArray[0].charAt(0);
+  var cssOutFixedSimple           = '';
+  
+  var i = 0;
+  
+  //Now iteration for attr fixed of json
   for (; objetive[i];) {
     $.each(confFixed.flex.width, function(keyWidth, valueWidth) {
       $.each(confFixed.flex.attr.rotate, function(keyRotate, valueRotate) {
+        var u = 0;
         for (; valueRotate[u];) {
-          classKey = Object.keys(confFixed)[0];
-          classWidth = keyWidth + 1;
-          classRotateKey = keyRotate;
-          classRotateValue = valueRotate[u];
-          classNameOutFlex = '.' + classKey + classWidth + toTitleCase(classRotateKey) + toTitleCase(classRotateValue.replace('-', '')) + toTitleCase(classFixed) + toTitleCase(objetive[i]) + '\n { \n';
-          classContentFlex = '\t' + classKey + ' : ' + classWidth + ';\n' + '\t' + classRotateKey + ' : ' + classRotateValue + ';\n' + '\t' + classFixed + ' : $' + objetive[i] + ';\n' + '\n } \n';
-          classNameOut = '<br\>.' + classRotateKey.toLowerCase() + toTitleCase(classRotateValue.replace('-', '')) + toTitleCase(classFixed) + toTitleCase(objetive[i]) + '\n { \n';
-          classContent = '\t' + classRotateKey + ' : ' + classRotateValue + ';\n' + '\t' + classFixed + ' : $' + objetive[i] + ';\n' + '\n } \n';
-          cssOutFlex = classNameOutFlex + classContentFlex;
-          cssOut = classNameOut + classContent;
-          print('.result', cssOutFlex + cssOut);
+            
+          //Save de name and attr of css in vars for build de finish output framework
+          classKey          = Object.keys(confFixed)[0];
+          classWidth        = keyWidth + 1;
+          classRotateKey    = keyRotate;
+          classRotateValue  = valueRotate[u];
+          
+          classNameOutFlex  = '.' + classWidth + toTitleCase(classRotateKey) + toTitleCase(classRotateValue.replace('-', '')) + classFixedSimple + objetive[i] + '\n{\n';
+          classContentFlex  = '\t' + classKey + ':' + classWidth + ';\n' + '\t' + classRotateKey + ':' + classRotateValue + ';\n' + '\t' + classFixed + ':' + objetive[i] + ';\n' + '\n}\n';
+          
+          classNameOut      = '<br\>.' + classRotateKey.toLowerCase() + toTitleCase(classRotateValue.replace('-', '')) + classFixedSimple + objetive[i] + '\n{\n';
+          classContent      = '\t' + classRotateKey + ':' + classRotateValue + ';\n' + '\t' + classFixed + ':' + objetive[i] + ';\n' + '\n}\n';
+          
+          if ( currentClass != classFixed + objetive[i] ) {
+            cssFixedSimple        = '<br\>.' + classFixedSimple + objetive[i] + '\n{\n';
+            cssContentFixedSimple = '\t' + classFixed + ':' + objetive[i] + ';\n' + '\n}\n';
+            cssOutFixedSimple = cssFixedSimple + cssContentFixedSimple;
+          }
+           
+          cssOutFlex        = classNameOutFlex + classContentFlex;
+          cssOut            = classNameOut + classContent;
+          
+          cssAll = ( currentClass != classFixed + objetive[i] ) ? cssOutFlex + cssOut + cssOutFixedSimple : cssOutFlex + cssOut;
+          
+          print('.result', cssAll);
+          
+          currentClass = classFixed + objetive[i];
           u++;
         }
         u = 0;
@@ -59,29 +115,6 @@ function recorrerObj(objetive, key) {
 
 function contructor() {
   recorrerJsonLevel(confFixed.flex.attr.fixed);
-}
-
-var confFixed = {
-  'flex': {
-    'width': ['1', '2'],
-    'attr': {
-      'rotate': {
-        'justifyContent': ['center'],
-        'alignContent': ['center'],
-        'flexDirection': ['row']
-      },
-      'fixed': {
-        'margin': {
-          'marginRight': ['middle'],
-          'marginLeft': ['middle'],
-          'marginTop': ['single', 'middle']
-        },
-        'padding': {
-          'padding': ['middle', 'single']
-        },
-      }
-    }
-  }
 }
 
 contructor();
